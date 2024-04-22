2045f26ae131ae3a40819f70505b8a89a373bc6c
Akash Ray <113591379+s371102@users.noreply.github.com>
2024-04-22 12:37:36 +0200

    Create trivy.sh
    Commands to execute the trivy tool

---
+++ trivy.sh
@@ -0,0 +1,8 @@
+sudo apt-get install wget apt-transport-https gnupg lsb-release
+
+wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | gpg --dearmor | sudo tee /usr/share/keyrings/trivy.gpg | sudo >
+
+echo "deb [signed-by=/usr/share/keyrings/trivy.gpg] https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main" | sudo te>
+
+sudo apt-get update
+sudo apt-get install trivy -y
